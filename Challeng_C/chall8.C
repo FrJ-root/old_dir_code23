e5a16ad277465e4d07918a877987d1d505bc5536
FrJallah <frj.mitnick@gmail.com>
2023-09-16 13:05:05 +0200

    All_Same

--- Challeng_C/chall8.C
+++ Challeng_C/chall8.C
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int nombre;
+	    int nombre;
 
-    printf("Entrez un nombre entier : ");
-    scanf("%d", &nombre);
+	        printf("Entrez un nombre entier : ");
+		    scanf("%d", &nombre);
 
-    printf("Valeur en octal : %o\n", nombre);
-    printf("Valeur en hexadecimal : %x\n", nombre);
+		        printf("Valeur en octal : %o\n", nombre);
+			    printf("Valeur en hexadecimal : %x\n", nombre);
 
-    return 0;
+			        return 0;
+}
