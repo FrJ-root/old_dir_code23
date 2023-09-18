a55f1b0974b515cf9e6c0f6364cd4c6816d921e6
FrJallah <frj.mitnick@gmail.com>
2023-09-18 17:42:28 +0200

    Boucles

--- Challeng_C/X-Les_Boucles/chall2.c
+++ Challeng_C/X-Les_Boucles/chall2.c
@@ -1,38 +1,14 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-bool estPremier(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    int nombreDeLignes;
-
-    printf("Veuillez entrer le nombre de lignes pour la pyramide : ");
-    scanf("%d", &nombreDeLignes);
-
-    int ligne = 1;
-    int etoiles = 1;
-
-    while (ligne <= nombreDeLignes) {
-        if (estPremier(etoiles)) {
-            for (int i = 1; i <= etoiles; i++) {
-                printf("*");
-            }
-            printf("\n");
-            ligne++;
-        }
-        etoiles++;
-    }
-
-    return 0;
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
 }
 
