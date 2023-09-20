2e8816ff46f3d09129d7577aa16f9ae7df4afbdc
FrJallah <frj.mitnick@gmail.com>
2023-09-20 16:29:49 +0200

    All_Same

--- Challeng_C/X-Travail_à_Faire/chall2.C
+++ Challeng_C/X-Travail_à_Faire/chall2.C
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
     float tem_fahrenheit, tem_celsius;
     char sensation[20];
     printf("temperature en Fahrenheit : ");
     scanf("%f", &tem_fahrenheit);
     tem_celsius = (tem_fahrenheit - 32) / 1.8;
-    if (tem_celsius <= 0) {
+
+
+    if(tem_celsius <= 0)
+    {
         printf("tres froid");
-    } else if (0< tem_celsius <= 10) {
+    }
+
+    else if(0< tem_celsius <= 10)
+    {
         printf("froid");
-    } else if (10< tem_celsius <= 30) {
+    }
+    else if(10< tem_celsius <= 30)
+    {
         printf("chaud");
-    } else {
+    }
+
+    else
+    {
         printf("tres chaud\n");
     }
-    printf("%.1f degres Fahrenheit equivaut a %.2f degres Celsius, ce qui est considere comme %s.\n", tem_fahrenheit, tem_celsius, sensation);
 
-    return 0;
+
+printf("%.1f degres Fahrenheit >> %.2f degres Celsius >> %s.\n", tem_fahrenheit, tem_celsius, sensation);
+
+return 0;
 }
-
-
