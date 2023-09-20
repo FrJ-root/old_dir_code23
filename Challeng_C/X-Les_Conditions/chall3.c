06fa553f7ffa28d02d03d538c192b50530c7f2cf
FrJallah <frj.mitnick@gmail.com>
2023-09-20 18:31:50 +0200

    Condition

--- Challeng_C/X-Les_Conditions/chall3.c
+++ Challeng_C/X-Les_Conditions/chall3.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int valeur1, valeur2;
-
-    printf("Entrez la première valeur : ");
-    scanf("%d", &valeur1);
-
-    printf("Entrez la deuxième valeur : ");
-    scanf("%d", &valeur2);
-
-    int somme = valeur1 + valeur2;
-
-    if (valeur1 == valeur2) {
-        int tripleSomme = 3 * somme;
-        printf("Les deux valeurs sont identiques, le triple de leur somme est : %d\n", tripleSomme);
-    } else {
-        printf("La somme des deux valeurs est : %d\n", somme);
-    }
+int main()
+{
+int  a,b;
+int  somme;
+printf("Entrer a : ");scanf("%d", &a);
+printf("Entrer b : ");scanf("%d", &b);
+somme=a+b;
+if(a==b){somme*=3;}
+printf("  la somme ->> %d\n", somme);
 
     return 0;
 }
-
